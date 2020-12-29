08af8d1321813ac85917e1d2f745166d5acf7900
Chinukads <72682150+Chinukads@users.noreply.github.com>
2020-12-29 09:58:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var canvas= new fabric.Canvas("myCanvas");
+var player_x= 10;
+var player_y= 10;
+var block_img_width= 30;
+var block_img_height= 30;
+var player_object= "OIP(1).jpg";
+var block_img_object= "";
+
+function player_update(){
+    fabric.Image.fromURL("OIP(1).jpg", function(Img){
+        player_object=Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top: player_y,
+            left: player_x
+        });
+        canvas.add(player_object);
+    });
+}
+
+function new_Image(get_Image){
+    fabric.Image.fromURL(get_Image, function(Img){
+    block_img_object=Img;
+    block_img_object.scaleToWidth(block_img_width);
+    block_img_object.scaleToHeight(block_img_height);
+    block_img_object.set({
+        top:player_y,
+        left:player_x
+    });
+    canvas.add(block_img_object);
+    });
+}
